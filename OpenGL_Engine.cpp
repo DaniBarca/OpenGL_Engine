874af26acc57c9c23845f36a13831c49426a8af0
Dani Barca Casafont <dani.barcasafont@gmail.com>
2016-04-25 00:20:12 +0200

    How to build for visual studio

--- OpenGL_Engine.cpp
+++ OpenGL_Engine.cpp
@@ -1,5 +1,25 @@
 // Blame @DaniBarca for this
 
+/*
+	# VisualStudio 2015 config:
+
+	> C/C++>General>Additional Include Directories: 
+		$(ProjectDir)lib\assimp-3.2\include;$(ProjectDir)lib\glfw-3.1.2\include;$(ProjectDir)lib\glm-0.9.7.9.3\glm;$(ProjectDir)lib\glew-1.9.0\include;%(AdditionalIncludeDirectories)
+
+	> Linker>General>Additional Library Directories
+		$(ProjectDir)lib\assimp-3.2\lib;$(ProjectDir)lib\glfw-3.1.2\lib-vc2015;$(ProjectDir)lib\glew-1.9.0\lib;%(AdditionalLibraryDirectories)
+
+	> Linker>General>Input
+		glew32.lib;opengl32.lib;glfw3.lib;assimp-vc130-mt.lib;
+
+	> Build Events>Post-Build Event>Command-line
+		copy "$(ProjectDir)lib\assimp-3.2\lib\assimp-vc130-mt.dll" "$(TargetDir)assimp-vc130-mt.dll"; 
+		copy "$(ProjectDir)lib\glew-1.9.0\bin\glew32.dll" "$(TargetDir)glew32.dll";
+		xcopy "$(ProjectDir)models" "$(TargetDir)models" /e /i /Y
+
+	!! To be built for x64 !!
+*/
+
 /*Include*/
 #include "Header.h"
 #include "Engine.h"
