bce9b8b046e4e06f2c5c49e1c47b99d38a3b4b9f
Daniel Barca <dani.barcasafont@gmail.com>
2016-12-13 17:28:29 +0100

    Revert "angle2 spot light"
    This reverts commit 477fd7c335b552d4de1365b31847ccf59feb5a9b.

--- SpotLight.h
+++ SpotLight.h
@@ -7,13 +7,12 @@ class SpotLight : public Light
 {
 	glm::vec3 direction;
 	float angle;
-	float angle_soft;
 
 public:
-	SpotLight(glm::vec4 _color, glm::vec3 _position, float _shininess,  float _intensity, glm::vec3 _direction, float _angle, float _angle_soft);
+	SpotLight(glm::vec4 _color, glm::vec3 _position, float _shininess,  float _intensity, glm::vec3 _direction, float _angle);
 	~SpotLight();
 
 	glm::vec3 GetDirection();
 	float GetAngle();
-	float GetAngleSoft();
 };
+
